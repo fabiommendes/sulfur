9e0ad26cd80568a2a06906e6c331945dd2448355
fabiomacedomendes <fabiomacedomendes@gmail.com>
2017-03-24 00:24:29 -0300

    tweak tox

--- src/sulfur_plugin/pytest.py
+++ src/sulfur_plugin/pytest.py
@@ -1,31 +1,34 @@
 """"
 Sulfur py.test plugin.
 """
-
-# We don't want to import sulfur here in order to preserve the coverage stats
-# for the sulfur package. If we import the sulfur module, parts of it will be
-# loaded before coverage starts its tracer, hence many lines will not be
-# counted when coverage runs.
+import os
 import pytest
 
 
-@pytest.fixture
-def driver(driver):
-    import sulfur
-    return sulfur.Driver()
+if not os.environ.get('SULFUR_DISABLE_PLUGIN', '').lower() == 'true':
+    # We don't want to import sulfur globally in order to preserve coverage
+    # stats for the sulfur package. If we import the sulfur module, parts of it will be
+    # loaded before coverage starts its tracer, hence many lines will not be
+    # counted when coverage runs.
 
 
-@pytest.fixture
-def client(driver):
-    import sulfur.client
-    return sulfur.Driver()
+    @pytest.fixture
+    def driver(driver):
+        import sulfur
+        return sulfur.Driver()
 
 
-@pytest.fixture
-def urlchecker():
-    """
-    Return the sulfur.urlchecker module.
-    """
+    @pytest.fixture
+    def client(driver):
+        import sulfur.client
+        return sulfur.Driver()
 
-    import sulfur.urlchecker
-    return sulfur.urlchecker
+
+    @pytest.fixture
+    def urlchecker():
+        """
+        Return the sulfur.urlchecker module.
+        """
+
+        import sulfur.urlchecker
+        return sulfur.urlchecker
